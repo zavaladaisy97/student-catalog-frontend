cd51352b5d678360712b75f0baa50fa2524fbf6a
Krystal Lopez <lopezkrystal10@gmail.com>
2022-08-22 19:31:09 -0700

    added index action

--- src/views/StudentCatalog.vue
+++ src/views/StudentCatalog.vue
@@ -1,18 +1,33 @@
 <script>
+// import axios from "axios";
+
 export default {
   data: function () {
     return {
       message: "Welcome to the Student Catalog!",
+      students: [{ first_name: "Bob", last_name: "Jones", capstone_name: "How to train your dragon" }],
     };
   },
-  created: function () {},
-  methods: {},
+  created: function () {
+    //   this.indexStudents();
+  },
+  methods: {
+    // indexStudents: function () {
+    //   axios.get("/students.json").then((response) => {
+    //     this.students = response.data;
+    //     console.log("All Students", response.data);
+    //   });
+  },
 };
 </script>
 
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+    <div v-for="student in students" v-bind:key="student.first_name">
+      <h2>{{ student.first_name }}</h2>
+      <p>{{ student.capstone_name }}</p>
+    </div>
   </div>
 </template>
 
